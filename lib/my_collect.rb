822356e5a45559dd5dc63d7ac2252faf64ab65ee
Sarah Jacob <smjacob91@gmail.com>
2018-07-06 10:11:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(collection)
+  if block_given?
+    new_collection = []
+    i = 0
+    while i < collection.length
+      new_collection << yield(collection[i])
+      i = i + 1
+    end
+  new_collection
+end
+    
+    
+    
